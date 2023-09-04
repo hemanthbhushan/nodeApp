4dbb040767745661bc4ea847ab503ab9754dec3e
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-09-04 16:55:10 +0530

    xx

--- src/controller/minerService.ts
+++ src/controller/minerService.ts
@@ -11,24 +11,9 @@ class BlockStore {
     try {
       const { latestBlockNumber, minerName } = req.body;
 
-      const existingBlock = await minerBlockSchema.findOne({
+      const existingBlock = await restaurantSchema.findOne({
         latestBlockNumber,
       });
-
-      if (!existingBlock) {
-        await minerBlockSchema.create({ latestBlockNumber, minerName });
-        console.log(`The latest block added to DB: ${latestBlockNumber}`);
-        res.status(200).json({
-          message: "BlockAdded",
-          blockNumber: latestBlockNumber,
-        });
-      } else {
-        console.log(`Block number ${latestBlockNumber} already exists`);
-        res.status(200).json({
-          message: "BlockExists",
-          blockNumber: latestBlockNumber,
-        });
-      }
     } catch (error) {
       console.error(error);
       res.status(500).json({
@@ -37,7 +22,6 @@ class BlockStore {
       });
     }
   }
-
 }
 
 export default new BlockStore();
